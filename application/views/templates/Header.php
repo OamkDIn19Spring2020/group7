cfa1b110aefba048a2b25aee9aa22add2170ba7b
Jami Hannus <t9haja00@students.oamk.fi>
2020-03-16 17:04:56 +0200

    Made some base changes, url helper activated

---
+++ application/views/templates/Header.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title> Group 7</title>
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <div>
+<!-- base url needs to be activated from helper file then you can use the base url to give -->
+            <div><a href="<?php echo base_url(); ?>">Home</a></div>
+            <div><a href="<?php echo base_url(); ?>about">About</a></div>
+        </div>
