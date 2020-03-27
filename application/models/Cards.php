bae728325d4be750be9a9bbe956fa396d5759ff6
AlexanderV <t9veal00@students.oamk.fi>
2020-03-27 14:55:36 +0200

    added cards model draft

---
+++ application/models/Cards.php
@@ -0,0 +1,50 @@
+<?php
+
+    class Cards extends CI_Model
+    {
+        public function __construct()
+        {
+            // Load CI database library
+            $this->load->database();
+        }
+        public function randomcardNumber();
+        {   
+            // construct random 12 digit number
+
+            $bar0 = 55;
+            $bar1 =  mt_rand (10,99);
+            $bar2 =  mt_rand (1000,9999);
+            $bar3 =  mt_rand (1000,9999);
+
+            $newNumber = $bar0.$bar1.$bar2.$bar3; 
+            return $newNumber;
+        }
+
+        //make a card
+        public function makeCard()
+
+        {
+
+        $newcardNumber = randomcardNumber();
+        
+        //check if card exists
+
+        $sqlfind = "SELECT * FROM CARDS WHERE cardID = '".$newcardNumber."';" ;
+        $result = $this->db->query($sqlfind);
+        
+        //if it doesnt
+        if ($result->num_rown < 1)
+        {
+
+            $data = [
+                'cardnumber' => $newcardNumber,
+                'credits' => '0',
+    ];
+
+        //create new card
+        return $this->db->insert('cards', $data);
+         }
+
+        }   
+
+    }
