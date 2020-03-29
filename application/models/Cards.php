2ad6113169a86386fc3037507b5f59c9b7bc37c0
AlexanderV <t9veal00@students.oamk.fi>
2020-03-30 01:09:03 +0300

    Fixed cards.php doesn't work still though.

--- application/models/Cards.php
+++ application/models/Cards.php
@@ -2,55 +2,64 @@
 
     class Cards extends CI_Model
     {
+
         public function __construct()
         {
             // Load CI database library
             $this->load->database();
         }
-        public function randomcardNumber();
+
+
+        public function randomcardNumber()
         {   
-            // construct random 12 digit number
+            // construct random 8 digit number
 
             $bar0 = 55;
             $bar1 =  mt_rand (10,99);
-            $bar2 =  mt_rand (1000,9999);
-            $bar3 =  mt_rand (1000,9999);
+            $bar2 =  mt_rand (10,99);
+            $bar3 =  mt_rand (10,99);
 
             $newNumber = $bar0.$bar1.$bar2.$bar3; 
             return $newNumber;
         }
 
         //make a card
+
+           
          public function makeCard()
         {
+           //check if card exists
+            $userid = $this->session->has_userdata('customer_id');
+            $query = $this->db->get_where('cards', array('cardnumber' => $newcardNumber));
+            $userquery = $this->db->get_where('cards',array('customer' => $userid));
 
-        $newcardNumber = randomcardNumber();
-        
-        //check if card exists
-
-        $sqlfind = "SELECT * FROM CARDS WHERE cardID = '".$newcardNumber."';" ;
-        $result = $this->db->query($sqlfind);
         
         //if it doesnt
-        if ($result->num_rown < 1)
-        {
+
+    
+        if ($query->num_rows() == 0 && $userquery->num_rows() == 0)
+          //create new card
+         {
+            $newcardNumber = randomcardNumber();
+        
 
             $data = [
                 'cardnumber' => $newcardNumber,
                 'credits' => '0',
+                'customer_id'=> $userid
             ];
 
-        //create new card
+      
         return $this->db->insert('cards', $data);
 
-         }
+        }
 
 
        
- } 
+ 
+        } 
 
-}
+    }
 
-?>
 
-  
+
