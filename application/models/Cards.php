c8c3d0d23b7bdd2dfcf6e61ab1cb74dbffb1903a
AlexanderV <t9veal00@students.oamk.fi>
2020-03-30 17:05:50 +0300

    fixed the loop inside cards.php

--- application/models/Cards.php
+++ application/models/Cards.php
@@ -9,42 +9,60 @@
             $this->load->database();
         }
 
+        // creat semi-random variable
+        
+        public function randomNum()
+        {
+            $bar0 = 11;
+            $bar1 =  mt_rand (10,99);
+            $bar2 =  mt_rand (1000,9999);
+            $newcardNumber = $bar0.$bar1.$bar2;
+            return $newcardNumber;
 
+        }
     
         //make a card
 
            
          public function makeCard()
         { 
+        
             
-            $bar0 = 5555;
-            $bar1 =  mt_rand (1000,9999);
 
-            
-            
-            $newcardNumber = $bar0.$bar1;
-           //check if card exists
+        //check if card exists
+
             $userid = $this->session->has_userdata('customer_id');
-            $query = $this->db->get_where('card', array('cardnumber' => $newcardNumber));
             $userquery = $this->db->get_where('card',array('customer_id' => $userid));
 
         
         //if it doesnt
 
     
-        if ($query->num_rows() == 0 && $userquery->num_rows() == 0)
-          //create new card
-         {
-          
-        
+        if ($userquery->num_rows() == 0)
+           
+            
+        //create new card
 
-            $data = [
+         {
+            $newcardNumber = $this->Cards->randomNum();
+
+            $query = $this->db->get_where('card', array('cardnumber' => $newcardNumber));
+        
+            while ($query->num_rows() > 0  )
+            {
+                 $newcardNumber = randomNum();
+                 $query = $this->db->get_where('card', array('cardnumber' => $newcardNumber));
+            }
+            
+            
+
+                  $data = [
                 'cardnumber' => $newcardNumber,
                 'credit' => 100,
                 'customer_id'=> $userid
             ];
 
-      
+        //push data into db
         return $this->db->insert('card', $data);
           }
      } 
