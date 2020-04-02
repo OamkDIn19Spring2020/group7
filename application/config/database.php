e38c093eb5063c3224bc23319007117cb5c6a97c
Jami Hannus <t9haja00@students.oamk.fi>
2020-04-02 12:38:31 +0300

    database name change

--- application/config/database.php
+++ application/config/database.php
@@ -74,15 +74,15 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'mysli.oamk.fi',
-	'username' => 't9abka00',
-	'password' => 'z42qt2EdED7yJEjy',
-	'database' => 'opisk_t9abka00',
+	'dsn'   => '',
+	'hostname' => 'localhost',
+	'username' => 'test',
+	'password' => 'test',
+	'database' => 'db',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
+	'pconnect' => TRUE,
+	'db_debug' => FALSE,
 	'cache_on' => FALSE,
 	'cachedir' => '',
 	'char_set' => 'utf8',
@@ -91,6 +91,28 @@ $db['default'] = array(
 	'encrypt' => FALSE,
 	'compress' => FALSE,
 	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
+	'failover' => array()
 );
+
+
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => 'mysli.oamk.fi',
+// 	'username' => 't9abka00',
+// 	'password' => 'z42qt2EdED7yJEjy',
+// 	'database' => 'opisk_t9abka00',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
