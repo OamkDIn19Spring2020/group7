9fea5a5ed39c6de0e495ef111612732cb75303a1
Jami Hannus <t9haja00@students.oamk.fi>
2020-04-23 09:52:17 +0300

    comment

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -5,6 +5,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
    <div class="mx-auto" style="width: 200px">
     &#169; <?php echo date("Y"); ?> Group 7
+    <!-- Maybe have github repo link too? Icon for github?-->
   </div>
   </body>
 </html>
