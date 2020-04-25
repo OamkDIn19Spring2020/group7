aeca5e3b02b516c357de988fc00443c83df99ff6
AlexanderV <t9veal00@students.oamk.fi>
2020-04-26 01:18:13 +0300

    index ui redone, minor text fixex. Added aboutus.

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -3,9 +3,14 @@
   <!-- Main jumbotron for a primary marketing message or call to action -->
   <div class="jumbotron">
     <div class="container">
-      <h1 class="display-3">City Card system</h1>
-      <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-      <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+      <h1 class="display-3">City Card System</h1>
+      <?php if($this->session->has_userdata('customer_id')) : ?>
+          <h2 class="display-4">  Welcome <?php echo $this->session->userdata('firstname'); ?></a></p>
+      </h2>
+      <p><a class="btn btn-primary btn-lg" href="<?php echo site_url('users/profile'); ?>"  role="button">View profile&raquo;</a></p>
+      <?php else : ?>
+        <p><a class="btn btn-primary btn-lg" href="<?php echo site_url('users/login'); ?>"  role="button">Sign in&raquo;</a></p>
+      <?php endif; ?>
     </div>
   </div>
 
@@ -13,19 +18,26 @@
     <!-- Example row of columns -->
     <div class="row">
       <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        <h2>Our products</h2>
+        <p>Browse our extended range of subscriptions. </p>
+        <p><a class="btn btn-secondary" href="<?php echo site_url('/subtypes'); ?>" role="button">View products &raquo;</a></p>
       </div>
       <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+
+      <?php if($this->session->has_userdata('customer_id')) : ?>
+        <h2>Card information</h2>
+        <?php echo $this->session->userdata('cardnumber'); ?>
+        <p>Check your card balance and review your purchases.</p> 
+      <p><a class="btn btn-secondary" href="<?php echo site_url('users/profile#'); ?>" role="button" >Go to My card &raquo;</a></p>
+      <?php else : ?>
+        <h2>Card information</h2>
+        <p><a class="btn btn-secondary" href="<?php echo site_url('users/login'); ?>"  role="button">Sign in&raquo;</a></p>
+      <?php endif; ?>
       </div>
       <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        <h2>About us</h2>
+        <p>Got any questions about us? We're no slim shady. Check out the about us page.</p>
+        <p><a class="btn btn-secondary" href="<?php echo site_url('abouts/aboutus'); ?>" role="button">About us &raquo;</a></p>
       </div>
     </div>
 
