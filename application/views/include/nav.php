14e37d50d9f36674c312891f38726431acdcb3c9
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2020-04-26 21:30:58 +0300

    Removed unnecessary code

--- application/views/include/nav.php
+++ application/views/include/nav.php
@@ -6,29 +6,28 @@
 
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-            <a class="nav-link" href="<?php echo site_url(); ?>">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item active">
-            <a class="nav-link" href="<?php echo site_url();?>/subtypes">Orders <span class="sr-only">(current)</span></a>
-        </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="<?php echo site_url(); ?>">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo site_url('SubTypes');?>">Orders </a>
+            </li>
             <?php if($this->session->has_userdata('customer_id')) : ?>
-            <li class="nav-item ">
-                               <a class="nav-link" href="<?php echo site_url('users/profile'); ?>">Welcome <?php echo $this->session->userdata('firstname'); ?></a>
-                          </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo site_url('users/profile'); ?>">Welcome <?php echo $this->session->userdata('firstname'); ?></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo site_url('users/logout'); ?>">Logout</a>
+            </li>
 
-                         <li class="nav-item ">
-                             <a class="nav-link" href="<?php echo site_url('users/logout'); ?>">Logout</a>
-                        </li>
-                               <?php else : ?>
-
-                                <li class="nav-item ">
-                                       <a class="nav-link" href="<?php echo site_url('users/register'); ?>">Register</a>
-                                </li>
-                              <li class="nav-item">
-                            <a class="nav-link" href="<?php echo site_url('users/login'); ?>">Login</a>
-                                </li>
-                           <?php endif; ?>
-    </ul>
-  </div>
+            <?php else : ?>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo site_url('users/register'); ?>">Register</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo site_url('users/login'); ?>">Login</a>
+            </li>
+            <?php endif; ?>
+        </ul>
+    </div>
 </nav>
