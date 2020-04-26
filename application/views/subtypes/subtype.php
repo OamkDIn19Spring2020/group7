a55973d7100b2be7d90a608aeb9758eabf56166b
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2020-04-26 21:31:30 +0300

    remove  = POST

--- application/views/subtypes/subtype.php
+++ application/views/subtypes/subtype.php
@@ -1,10 +1,7 @@
 <?php include(APPPATH . '/views/include/header.php'); ?>
 <?php include(APPPATH . '/views/include/nav.php'); ?>
-<!-- Should save post action in array in session what can be called -->
-<?php $_SESSION['postdata'] = $_POST;?>
 
-<div class="container">
-    <div class="container p-1">
+<div class="container mt-4">
         <div class="card-columns">
             <?php foreach ($subtype as $row)
             {
@@ -14,13 +11,15 @@
                         <!-- Tile -->
                         <h5 class="card-title"><?php echo $row['name']; ?></h5>
                         <!-- Here we have bit fader text -->
-                        <h6 class="card-subtitle mb-2 text-muted"><?php echo 'Id is '.$row['subtype_id'].' Price is : '.$row['cost'].' per month'; ?></h6>
+                        <h6 class="card-subtitle mb-2"><?php echo '<b>Price</b>: '. $row['cost'] .' credits per month'; ?></h6>
                         <!-- Description -->
-                        <p class="card-text-center"> <?php echo '<b>Description</b>: '.$row['description'] ?></p>
-                        <p class="card-text"><?php echo '<b>Info</b> '.$row['address']; ?></p>
+                        <p class="card-text-center"> <?php echo '<b>Description</b>: ' . $row['description'] ?></p>
+                        <p class="card-text"><?php echo '<b>Info</b> ' . $row['address']; ?></p>
                         <!-- Form start to catch users chosen sub's Id -->
-                        <form action="BuyPages" method="post">
-                            <input type="hidden" name="Id_Button" value="<?php echo $row['subtype_id'] ?>">
+                        <?php echo form_open('Orders/index', 'method="get"'); ?>
+                            <input type="hidden" name="subtype_id" value="<?php echo $row['subtype_id'] ?> ">
+                            <input type="hidden" name="name" value="<?php echo $row['name'] ?> ">
+                            <input type="hidden" name="cost" value="<?php echo $row['cost'] ?> ">
                             <input class="btn btn-primary" type="submit">
                         </form>
                     </div>
