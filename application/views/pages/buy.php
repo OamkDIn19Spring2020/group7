668bfc32d6b3b63121ca0100b90c3c32d44a1559
AlexanderV <t9veal00@students.oamk.fi>
2020-04-26 22:00:00 +0300

    Index layout + profile layout

--- application/views/pages/buy.php
+++
@@ -1,9 +0,0 @@
-<?php include(APPPATH . '/views/include/header.php'); ?>
-<?php include(APPPATH . '/views/include/nav.php'); ?>
-
-
-Here Be the money making page
-
-
-
-<?php include(APPPATH . '/views/include/footer.php'); ?>
